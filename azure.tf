4d7f478c1a839724f391b8b5dd16ce65d389da09
Skylar Onstot <skylaronstot@gmail.com>
2022-04-09 16:19:54 -0700

    Update azure.tf

--- azure.tf
+++ azure.tf
@@ -20,7 +20,7 @@ resource "azurerm_container_group" "techchallenge-app" {
   location            = azurerm_resource_group.techchallenge-app.location
   resource_group_name = azurerm_resource_group.techchallenge-app.name
   ip_address_type     = "Public"
-  dns_name_label      = "aci-label"
+  dns_name_label      = "techchallengetestapp"
   os_type             = "Linux"
 
 
